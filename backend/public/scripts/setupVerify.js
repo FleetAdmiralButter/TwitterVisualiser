7f8831fba7b54cee5255e67039cd759300ca010e
Ming S Quah <quahmingsheng@me.com>
2020-08-21 23:34:20 +1000

    Fix package-lock

--- backend/public/scripts/setupVerify.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-	var elements = document.querySelectorAll("select");
-	var instances = M.FormSelect.init(elements);
-});
--- backend/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in $(find . -path ./node_modules -prune -o -name "*.js" -print); 
-	do node --check $i;
-	if [ "$?" -gt 0 ]
-	then
-		exit 1; 
-	fi
-done
