b31d8f7e86eb3be64646c7dd479bd090c2c5dda4
Ming S Quah <quahmingsheng@me.com>
2020-08-22 17:26:41 +1000

    Add dockerfiles

--- controllers/mongoController.js
+++ controllers/mongoController.js
@@ -4,10 +4,10 @@ const crime = require('../models/crime_schema');
 const ObjectId = require('mongodb').ObjectId;
 const spawn = require('threads').spawn;
 const connectionTimeout = 1500;
-const databaseMelb = {location: "Melbourne", url : "mongodb://team:swinburne@43.240.97.166/tweets", type: "Production"};
-const databaseChicago = {location: "Chicago", url : "mongodb://team:swinburne@43.240.97.166/tweetsChicago", type: "Production"};
-const databaseChicagoCrime = {location:"Chicago Crime", url : "mongodb://team:swinburne@43.240.97.166/chicagoCrime", type: "Production"};
-const connectFailure = function() {console.log("This will abort the NodeJS process."); process.exit(1);}
+const databaseMelb = {location: "Melbourne", url : "mongodb://team:swinburne@136.186.108.40/tweets", type: "Production"};
+const databaseChicago = {location: "Chicago", url : "mongodb://team:swinburne@136.186.108.40/tweetsChicago", type: "Production"};
+const databaseChicagoCrime = {location:"Chicago Crime", url : "mongodb://team:swinburne@136.186.108.40/chicagoCrime", type: "Production"};
+const connectFailure = function() {console.log("This will abort the NodeJS process.");}
 
 init();
 
